a28d27eee3b32cf69933c64a2504b1bbc25818bd
dj456119 <362088785@qq.com>
2021-11-30 00:03:53 +0800

    support:create writer buff util

--- wal_util.go
+++ wal_util.go
@@ -4,7 +4,7 @@
  * @Author: cm.d
  * @Date: 2021-11-20 11:47:46
  * @LastEditors: cm.d
- * @LastEditTime: 2021-11-20 12:09:23
+ * @LastEditTime: 2021-11-29 23:52:12
  */
 
 package alfheimdbwal
@@ -20,6 +20,24 @@ func NewLogItemBuff(index int64, data []byte, buff []byte, isBigEndian bool) *Lo
 	lItem.Index = index
 	lItem.Length = uint64(length)
 	return lItem
+}
+
+func CreateWriteBuff(writeBuff []byte, exec func(args ...interface{}) (int64, []byte), args ...interface{}) (*LogItem, []byte) {
+	index, buff := exec(args)
+	lItem := NewLogItemBuff(index, buff, writeBuff, true)
+	return lItem, writeBuff[:8+8+len(buff)]
+}
+
+func CreateBatchWriteBuff(batchWriteBuff []byte, execs []func(args ...interface{}) (int64, []byte), args ...[]interface{}) ([]*LogItem, []byte) {
+	pos := 0
+	lItems := make([]*LogItem, len(execs))
+	for i, exec := range execs {
+		index, buff := exec(args[i])
+		lItem := NewLogItemBuff(index, buff, batchWriteBuff[pos:], true)
+		lItems[i] = lItem
+		pos = pos + 8 + 8 + len(buff)
+	}
+	return lItems, batchWriteBuff[:pos]
 }
 
 func WriteInt64ToBuff(buff []byte, data int64, isBigEndian bool) {
